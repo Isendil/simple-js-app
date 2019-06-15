aff9760c443b51dd848cb75df190328f14ae2e16
Isendil <Isendil@github.com>
2019-06-14 23:42:10 -0400

    change fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,52 @@
-var repository =
-{
-  name: 'Bullbasur', size: '0.7',
-type: 'Grass', 'Poison'
-}
-{
-  name: 'Charmander', size: '0.6',
-type: 'Fire', 'Burn',
-}
-{
-  name: 'Squirtle', size: '0.5',
-  type: 'Water', 'Hydro pump'
-},;
-repository.forEach(function(currentName)){
-  console.log(currentName)
-}
+var repository = [
+  ['Bullbasur',
+  0.7,
+  ['Grass', 'Poison']],
+
+  ['Charmander', 0.6,
+  ['Fire', 'Burn']],
+
+  ['Squirtle',
+  0.5,
+  ['Water', 'hydro pump']],
 ]
 
-console.log(repository[1][0]);
 
-// Loop for each item
+
+console.log (repository[1][0]);
+
+
 for (var i = 0; i < repository.length; i++) {
-  if (repository[i][1] > 1.1) {
-    document.write ('<p class="top_article">Wow - he is the biggest Pokemon</p>');
+  if (repository[i][1] > 1.9) {
+    document.write ('<p class="top_article">Wow - he is biggest Pokemon</p>');
   }
+
+  document.write ('<H2><a href="">' + repository[i][0] + '</a></H2>');
+
+  document.write ('<p class="p__small">Types: ');
+  for (var j = 0; j < repository[i][2].length; j++) {
+    if (repository[i][2][j] == 'Grass') {
+      document.write (' <span class="p__small" style="color:rgb(0, 100, 0);">' + repository[i][2][j] + ',');
+    }
+    else if (repository[i][2][j] == 'Poison') {
+      document.write (' <span class="p__small" style="color:rgb(50, 205, 50);">' + repository[i][2][j] + ',');
+    }
+    else if (repository[i][2][j] == 'Fire') {
+      document.write (' <span class="p__small" style="color:rgb(139, 0, 0);">' + repository[i][2][j] + ',');
+    }
+    else if (repository[i][2][j] == 'Burn') {
+      document.write (' <span class="p__small" style="color:rgb(128, 0, 0);">' + repository[i][2][j] + ',');
+    }
+
+    else if (repository[i][2][j] == 'Water') {
+    document.write (' <span class="p__small" style="color:rgb(0, 0, 128);">' + repository[i][2][j] + ',');
+  }
+
+  else if (repository[i][2][j] == 'hydro pump') {
+  document.write (' <span class="p__small" style="color:rgb(0, 0, 255);">' + repository[i][2][j] + ',');
 }
 
-document.write ('<h3><a href="">' + repository[i][0] + '</a></h3>');
-
-document.write ('<p class="p_poke">Types: ');
-for (var j = 0; j < repository[i][2].length; j++) {
-  if (repository[i][2][j] == 'Grass') {
-    document.write (' <span class="p_poke";' + repository[i][2][j] + ',');
   }
-  else if (repository[i][2][j] == 'Poison'){
-    document.write (' <span class="p_poke";' + repository[i][2][j] + ',');
-  }
-  else if (repository[i][2][j] == 'Fire'){
-    document.write (' <span class="p_poke";' + repository[i][2][j] + ',');
-  }
-  else if (repository[i][2][j] == 'Burn'){
-    document.write (' <span class="p_poke";' + repository[i][2][j] + ',');
-  }
-  else if (repository[i][2][j] == 'Water'){
-    document.write (' <span class="p_poke";' + repository[i][2][j] + ',');
-  }
-  else if (repository[i][2][j] == 'Hydro pump'){
-    document.write (' <span class="p_poke";' + repository[i][2][j] + ',');
-  }
+  document.write ('</p>');
+  document.write ('<BR><BR>');
 }
-document.write ('</p>');
-document.write ();
