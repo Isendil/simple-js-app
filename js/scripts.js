e6caaaf478db773856c070bb58f33cc970dc7829
Isendil <Isendil@github.com>
2019-06-27 15:26:08 -0400

    Changes
    a lot of typos... from what I figure out most of my mistakes happen are because of typos.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 var repository = [
   {
     name: "Bulbasaur",
@@ -6,7 +7,7 @@ var repository = [
   },
   {
     name: "Charmander",
-    height: 1.1,
+    height: 1.7,
     types: ["fire"]
   },
   {
@@ -17,19 +18,18 @@ var repository = [
 ];
 
 
-
-
 for (var i = 0; i < repository.length; i++) {
   var result;
   for (var j = 0; j < repository[i].types.length; j++) {
     if (repository[i].types[j] == "grass") {
       result = '<span style="color:green;"> ';
-    } else if (repository[i].type[j]  == "fire") {
+    } else if (repository[i].types[j]  == "fire") {
       result = '<span style="color:red;"> ';
-    } else if (repository[i].type[j]  == "water") {
+    } else if (repository[i].types[j]  == "water") {
       result = '<span style="color:blue;"> ';
     }
   }
+
 
   var size;
   if (repository[i].height > 1) {
@@ -38,9 +38,15 @@ for (var i = 0; i < repository.length; i++) {
     size = "Is a small";
   }
 
+
   document.write(
-    '<div class = "box">' + repository[i].name + " " +
-    "(Height: " + repository[i].height + "m" + ")" +
+    '<div class = "box">' +
+    repository[i].name +
+     " " +
+    "(Height: " +
+    repository[i].height +
+    "m" +
+    ")" +
     "<br>" +
     repository[i].types +
     "<br>" +
--- js/testscript.js
+++ js/testscript.js
@@ -1,53 +1,55 @@
+
 var repository = [
   {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "dirt"]
-  },
-  {
-    name: "Charmander",
-    height: 1.1,
-    types: ["fire"]
-  },
-  {
-    name: "Squirtle",
+    name: 'Bullbasar',
     height: 0.5,
-    types: ["water"]
-  }
+    type: ['Grass', 'Poison'],
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    type: ['Fire', 'Burn'],
+  },
+  {
+    name: 'Squirtle',
+    height: 0.7,
+    type: ['Water', 'Hydro'],
+  },
 ];
 
+/* checking on console if the names types and heights are working */
+console.log(repository[2]);
 
+document.write(repository);
 
-
-for (var i = 0; i < repository.length; i++) {
-  var result;
-  for (var j = 0; j < repository[i].types.length; j++) {
-    if (repository[i].types[j] == "grass") {
-      result = '<span style="color:green;"> ';
-    } else if (repository[i].type[j]  == "fire") {
-      result = '<span style="color:red;"> ';
-    } else if (repository[i].type[j]  == "water") {
-      result = '<span style="color:blue;"> ';
-    }
+/* for loop */
+for (i = 0; i < repository[i].length; i++); {
+  var pokemon = repository[i];
+  if (repository[i] > 0.6) {
+    document.write('<p>Wow he is the biggest pokemon </p>');
   }
 
-  var size;
-  if (repository[i].height > 1) {
-    size = "Wow, that is big!";
-  } else {
-    size = "Is a small";
-  }
+if (repository[i] === 'Grass') {
+  document.write('<span style="color:rgb(0, 100, 0);">' + repository.name + ',');
+}
+else if (repository[i] === 'Poison') {
+  document.write('<span style="color:rgb(50, 205, 50);">' + repository[i] + ',');
+}
+else if (repository[i] === 'Fire') {
+  document.write ('<span style="color:rgb(139, 0, 0);">' + repository[i] + ',');
+}
+else if (repository[i] === 'Burn') {
+  document.write ('<span style="color:rgb(128, 0, 0);">' + repository[i] + ',');
+}
+else if (repository[i] === 'Water') {
+  document.write ('<span style="color:rgb(0, 0, 128);">' + repository[i] + ',');
+}
+else if (repository[i] == 'Hydro') {
+  document.write ('<span style="color:rgb(0, 0, 255);">' + repository[i] + ',');
+}
 
-  document.write(
-    '<div class = "box">' + repository[i].name + " " +
-    "(Height: " + repository[i].height + "m" + ")" +
-    "<br>" +
-    repository[i].types +
-    "<br>" +
-    result +
-    "<br>" +
-    size +
-    "</div>" +
-    "<br>"
-  );
+document.write('<h2>>' + repository.name + '</h2>');
+document.write('<p>Type: </p>');
+document.write('<p>');
+document.write('<br>');
 }
