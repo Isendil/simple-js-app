49f3d610dfe57cdc8c6851deb167d293c855133c
Isendil <Isendil@github.com>
2019-06-29 18:28:11 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
+/* here goes the var, arrays, objects with strings inside them */
 var repository = [
   {
     name: 'Pikachu',
     height: 0.4,
-    types: ['Electric']
+    types: ['electric']
   }, {
     name: 'Torterra',
     height: 2.2,
-    types: ['Grass', 'Ground']
+    types: ['grass', 'ground']
   }, {
     name: 'Ninetales',
     height: 1.1,
-    types: ['Fire']
+    types: ['fire']
   }, {
     name: 'Metalgross',
     height: 1.6,
-    types: ['Psychic', 'Steel']
+    types: ['psychic', 'steel']
   }, {
     name: 'Pidove',
     height: 0.3,
-    types: ['Flying']
+    types: ['flying']
   } ];
+   console.log(repository)
   
+   var pokemonRepository = (function () {
+    var repository = []; // empty array
+  
+    return {
+      add: function(pokemon) {
+        repository.push(pokemon);
+      },
+      getAll: function() {
+        return repository;
+      }
+    };
+  })();
+  
+  console.log(pokemonRepository.getAll()); // []
+  pokemonRepository.add({ name: 'Pikachu' });
+  console.log(pokemonRepository.getAll());
+  
+   /* Here is our loop */
   for (var i = 0; i < repository.length; i++) {
     var size;
     if (repository[i].height > 1) {
@@ -28,17 +48,45 @@ var repository = [
   } else {
     size = "It is a small Pokemon";
   }
+  
+  var result;
+  for (var j = 0; j < repository[i].types.length; j++) {
+    if (repository[i].types[j] == 'electric') {
+      result = '<span style="color:yellow;"> ';
+    } else if (repository[i].types[j] == 'ground') {
+      result = '<span style="color:rgb(0, 100, 0);"> ';
+    } else if (repository[i].types[j] == 'fire') {
+      result = '<span style="color:red;"> ';
+    } else if (repository[i].types[j] == 'psychic') {
+      result = '<span style="color:yellow;"> ';
+    } else if (repository[i].types[j] == 'steel') {
+      result = '<span style="color:rgb(199, 21, 133);"> ';
+    } else if (repository[i].types[j] == 'flying') {
+      result = '<span style="color:blue;"> ';
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  /* Here we ask we write what we want to see on our browser */
   document.write(
     '<div class="box">' +
     repository[i].name +
     "(height: " +
-    repository[i].height + "m" + ")" +
-    '<br>'
+    repository[i].height +
+    "m" +
+    ")" +
+    "<br>" +
     size +
     result +
-    '<br>' + 
+    "<br>" +
     repository[i].types +
-    '<br>' +
-    '</div>'
-    );
+    "<br>" +
+    "</div>"
+  );
   }
