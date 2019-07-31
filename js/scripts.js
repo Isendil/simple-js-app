e902849349e43df2140b7cc4508949137e308f99
Isendil <Isendil@github.com>
2019-07-30 20:44:50 -0400

    changes 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,23 @@
 
-var pokemonRepository = (function () {
+var pokemonRepository = (function() {
   var repository = [];
-  var apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  var apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20";
 
-  // function add  specifically adds whatever is outside the code to access it.
   function add(pokemon) {
     if (
-      typeof pokemon === 'object' &&
-      'name' in pokemon &&
-      'detailsUrl' in pokemon
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon
     ) {
       repository.push(pokemon);
     } else {
-      console.log('add and object');
+      console.log("add an object");
     }
   }
-  // function that can get what is written outside the IIFE code and access it.
   function getAll() {
     return repository;
   }
-  // created a new function and connected with the index.html, created variables and inside new elements with li and button
-  // and after we connected the text inside the button to be the names of each pokemon. created a classList and connected
-  // with css to style it. after we created the button and put an order in the list, an event click with function event
-  // was created and inside we defined a parameter so when we click it the pokemon informations to be shown.
-  function addListItem(pokemon = {}) {
+  function addListItem(pokemon) {
     var pokemonList = document.querySelector(".pokemon-list");
     var $listItem = document.createElement("li");
     var button = document.createElement("button");
@@ -31,24 +25,23 @@ var pokemonRepository = (function () {
     button.classList.add("my-class");
     $listItem.appendChild(button);
     pokemonList.appendChild($listItem);
-    button.addEventListener('click', function (event) {
+    button.addEventListener("click", function(event) {
       showDetails(pokemon);
-
     });
   }
-
   function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      // console.log(item)
+    pokemonRepository.loadDetails(item).then(function() {
+      console.log(item);
+      showModal(item);
     });
   }
   function loadList() {
     return fetch(apiUrl)
-      .then(function (response) {
+      .then(function(response) {
         return response.json();
       })
-      .then(function (json) {
-        json.results.forEach(function (item) {
+      .then(function(json) {
+        json.results.forEach(function(item) {
           var pokemon = {
             name: item.name,
             detailsUrl: item.url
@@ -57,7 +50,7 @@ var pokemonRepository = (function () {
           console.log(pokemon);
         });
       })
-      .catch(function (e) {
+      .catch(function(e) {
         console.error(e);
       });
   }
@@ -65,31 +58,149 @@ var pokemonRepository = (function () {
   function loadDetails(item) {
     var url = item.detailsUrl;
     return fetch(url)
-      .then(function (response) {
+      .then(function(response) {
         return response.json();
       })
-      .then(function (details) {
+      .then(function(details) {
+        // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = Object.keys(details.types);
+        //loop for each ofthe pokemon types.
+        //Also changing the background color depend on each pokemon type.
+        item.types = [];
+        for (var i = 0; i < details.types.length; i++) {
+          item.types.push(details.types[i].type.name);
+          if (details.types[i].type.name == "grass") {
+            document.getElementById("modal-container").style.background =
+              "lightgreen";
+          } else if (details.types[i].type.name == "fire") {
+            document.getElementById("modal-container").style.background = "red";
+          } else if (details.types[i].type.name == "psychic") {
+            document.getElementById("modal-container").style.background =
+              "yellow";
+          } else if (details.types[i].type.name == "poison") {
+            document.getElementById("modal-container").style.background =
+              "purple";
+          } else if (details.types[i].type.name == "water") {
+            document.getElementById("modal-container").style.background =
+              "blue";
+          } else if (details.types[i].type.name == "bug") {
+            document.getElementById("modal-container").style.background =
+              "#3f000f";
+          }
+        }
+        //loop to get the abilities of a selected pokemon
+        item.abilities = [];
+        for (var i = 0; i < details.abilities.length; i++) {
+          item.abilities.push(details.abilities[i].ability.name);
+         // item.abilities.push('slot: ' + details.abilities[i].slot);
+         // item.abilities.push('is_hidden: ' + details.abilities[i].is_hidden);
+        }
+        
+        item.weight = details.weight;
       })
-      .catch(function (e) {
+      .catch(function(e) {
         console.error(e);
       });
   }
-  //function return with the keys add and getAll what we have defined and ask it specificaly, in this case names etc...
+  // show the modal content
+  function showModal(item) {
+    var $modalContainer = document.querySelector("#modal-container");
+
+    //clear existing content of the model
+    $modalContainer.innerHTML = "";
+
+    //creating div element in DOM
+    var modal = document.createElement("div");
+    //adding class to div DOM element
+    modal.classList.add("modal");
+
+    //creating closing button in modal content
+    var closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = "Close";
+
+    // adding event listener to close modal when clicked on button
+    closeButtonElement.addEventListener("click", hideModal);
+
+    //creating element for name in modal content
+    var nameElement = document.createElement("h1");
+    nameElement.innerText = item.name;
+
+    // creating img in modal content
+    var imageElement = document.createElement("img");
+    imageElement.classList.add("modal-img");
+    imageElement.setAttribute("src", item.imageUrl);
+
+    //creating element for height in modal content
+    var heightElement = document.createElement("p");
+    heightElement.innerText = "height : " + item.height;
+
+    //creating element for weight in modal content
+    var weightElement = document.createElement("p");
+    weightElement.innerText = "weight : " + item.weight;
+
+    //creating element for type in modal content
+    var typesElement = document.createElement("p");
+    typesElement.innerText = "types : " + item.types;
+
+    //creating element for abilities in modal content
+    var abilitiesElement = document.createElement("p");
+    abilitiesElement.innerText = "abilities : " + item.abilities;
+
+    //appending modal content to webpage
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(nameElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(weightElement);
+    modal.appendChild(typesElement);
+    modal.appendChild(abilitiesElement);
+    $modalContainer.appendChild(modal);
+
+    //adds class to show the modal
+    $modalContainer.classList.add("is-visible");
+  }
+
+  //hides modal when clicked on close button
+  function hideModal() {
+    var $modalContainer = document.querySelector("#modal-container");
+    $modalContainer.classList.remove("is-visible");
+  }
+
+  //hides modal when clicked on ESC on keyboard
+  window.addEventListener("keydown", e => {
+    var $modalContainer = document.querySelector("#modal-container");
+    if (
+      e.key === "Escape" &&
+      $modalContainer.classList.contains("is-visible")
+    ) {
+      hideModal();
+    }
+  });
+
+  //hides modal if clicked outside of it
+  var $modalContainer = document.querySelector("#modal-container");
+  $modalContainer.addEventListener("click", e => {
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
   };
 })();
 
-pokemonRepository.loadList()
-  .then(function () {
-    pokemonRepository.getAll().forEach(function(pokemon) {
-      pokemonRepository.addListItem(pokemon);
-    });
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
   });
+});
